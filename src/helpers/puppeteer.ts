fc787248d034c1a93e9a48fbc809fe26330a782f
root <root@vps.server.local>
2022-11-13 23:07:56 +0000

    Make more suitable for deployment on VPS

--- src/helpers/puppeteer.ts
+++ src/helpers/puppeteer.ts
@@ -3,7 +3,9 @@ import { launch, Browser } from "puppeteer";
 let cachedBrowser: Browser | null = null;
 
 export const getBrowser = async () => {
-  if (!cachedBrowser) cachedBrowser = await launch();
+  if (!cachedBrowser) cachedBrowser = await launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']	
+  });
 
   return cachedBrowser;
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 
@@ -5,12 +6,20 @@ import { sumupRouter } from "./routers/sumup.router";
 
 export const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
   console.log("The server is up and running on port " + port);
 });
 
+app.use(express.static(path.join(__dirname, "../static"), {
+  dotfiles: "allow"
+}));
+
 app.use(cors());
 
 app.use("/sumup", sumupRouter);
+
+app.use("/*", (req, res) => {
+  return res.status(404).send("You are lost in the beautiful universe of the web");
+})
