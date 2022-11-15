921d6812fb07054e8d1c69e16dbaccf563dca9c0
Taouarda Ayoub <ayoubtrdone@gmail.com>
2022-11-15 20:23:35 +0100

    Attempt to speed up process for generating checkout url

--- src/services/scraping.service.ts
+++ src/services/scraping.service.ts
@@ -17,20 +17,34 @@ export class ScrapingService {
   async getSumUpBuyLink(productUrl: string): Promise<string | null> {
     if (!this.browser) await this.init();
 
+    console.time("Create incognito context");
     const context = await this.browser!.createIncognitoBrowserContext();
-    const page = await context.newPage();
+    console.timeEnd("Create incognito context");
 
+    console.time("New page");
+    const page = await context.newPage();
+    console.timeEnd("New page");
+
+    console.time("Open product page");
     await page.goto(productUrl, {
       waitUntil: "networkidle2",
     });
+    console.timeEnd("Open product page");
 
+    console.time("Click on checkout button");
     await page.click("#go-to-checkout");
-    await sleep(800);
+    console.timeEnd("Click on checkout button");
+    await sleep(2000);
 
     let url = page.url();
 
     if (!url.includes("checkout")) {
+      console.log("Checkout url not ready: ", url);
+
+      console.time("Navigation to checkout page");
       await page.waitForNavigation({ waitUntil: "networkidle2" });
+      console.timeEnd("Navigation to checkout page");
+
       url = page.url();
     }
 
