56fb51e3d6791a87e2acf79444c0b24a7c5b1fda
root <root@vps.server.local>
2022-11-15 18:44:44 +0000

    Apply changes required for https

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,24 +1,25 @@
 import path from "path";
+import fs from "fs";
+import https from "https";
+
 import express from "express";
+import morgan from "morgan";
 import cors from "cors";
 
 import { sumupRouter } from "./routers/sumup.router";
 
 export const app = express();
 
-const port = process.env.PORT || 80;
+app.use(cors());
+app.use(morgan("short"));
 
-app.listen(port, () => {
-  console.log("The server is up and running on port " + port);
-});
+console.log("Serving static files from: ", path.join(__dirname, "../../static"))
 
 app.use(
   express.static(path.join(__dirname, "../../static"), {
     dotfiles: "allow",
   })
 );
-
-app.use(cors());
 
 app.use("/sumup", sumupRouter);
 
@@ -27,3 +28,22 @@ app.use("/*", (req, res) => {
     .status(404)
     .send("You are lost in the beautiful universe of the web");
 });
+
+const port = process.env.PORT || 80;
+
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/api.trdsoftwares.com/privkey.pem");
+const fullChain = fs.readFileSync("/etc/letsencrypt/live/api.trdsoftwares.com/fullchain.pem");
+
+if (privateKey && fullChain) {
+  console.log("Starting up a production https server....");
+  https.createServer({
+    key: privateKey,
+    cert: fullChain
+  }, app).listen(443, () => {
+    console.log("The server is up and running on port ", 443);
+  });
+} else {
+  app.listen(port, () => {
+    console.log("The server is up and running on port: ", port);
+  })
+}
